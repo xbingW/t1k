ecc683157f45903b2dc9611d44c21303d8437e5e
xiaobing.wang <xiaobing.wang@chaitin.com>
2024-06-05 18:33:40 +0800

    init

---
+++ examples/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/xbingW/t1k/detector"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		d, err := detector.NewDetector(detector.Config{
+			Addr: os.Getenv("DETECTOR_ADDR"),
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res, err := d.DetectorRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if !res.Allowed() {
+			http.Error(w, "blocked", http.StatusForbidden)
+			return
+		}
+		_, _ = w.Write([]byte("allowed"))
+	})
+	_ = http.ListenAndServe(":80", nil)
+}
